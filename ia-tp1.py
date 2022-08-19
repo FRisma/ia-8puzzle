ae398ee89550bc55f053b66fa45a87cac1b4d564
Daro <daro@Daros-MacBook-Pro.local>
2022-08-19 15:29:54 -0300

    Reorder code.

--- ia-tp1.py
+++ ia-tp1.py
@@ -1,57 +1,54 @@
+from models import Node
 import copy
 
-initialConfig = [
-    [1,2,3],
-    [5,0,6],
-    [4,7,8]
-]
-goalConfig = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,0]
+initial_config = [
+    [1, 2, 3],
+    [5, 0, 6],
+    [4, 7, 8]
 ]
 
-class Node():
-    def __init__(self, config=None, historical=[]):
-        self.config = config
-        self.historical = historical
+goal_config = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 0]
+]
 
 
 def start():
     # Dado un estado inicial
     # Obtengo todos los posibles configs
     
-    initialNode = Node(config=initialConfig, historical=[])
-    recursiveCheck([initialNode])
+    initial_node = Node(config=initial_config, historical=[])
+    recursive_check([initial_node])
     
 
-def recursiveCheck(nodes):
-    childrenNodes = []
+def recursive_check(nodes):
+    children_nodes = []
     for node in nodes:
-        subNodes = getChilderNodes(node)
-        for subNode in subNodes:
+        sub_nodes = get_children_nodes(node)
+        for sub_node in sub_nodes:
             # Chequear si es igual que algun antecesor para descartarlo
             # if isSameConfig(initialChild, config):
             #     print('continue')
             #     continue
         
-            childrenNodes.append(subNode)
+            children_nodes.append(sub_node)
 
-    isSolution, possibleNode = checkIfAnyNodeIsSolution(childrenNodes)
-    if isSolution:
-        print("Hemos encontrado el resultado y es ", possibleNode.historical, possibleNode.config)
+    is_solution, possible_node = check_any_node_is_solution(children_nodes)
+    if is_solution:
+        print("Hemos encontrado el resultado y es ", possible_node.historical, possible_node.config)
     else:
-        recursiveCheck(childrenNodes)
+        recursive_check(children_nodes)
 
-def getChilderNodes(node):
-    childrenNodes = []
-    configs = getChildrenConfigs(node.config)
+
+def get_children_nodes(node):
+    children_nodes = []
+    configs = get_children_configs(node.config)
     for config in configs:
         print('historical', node.historical)
-        newHistorical = None
-        newHistoricalCopy = copy.deepcopy(node.historical)
-        newHistoricalCopy.append(node.config)
-        print('newHistoricalCopy', newHistoricalCopy)
+        new_historical_copy = copy.deepcopy(node.historical)
+        new_historical_copy.append(node.config)
+        print('newHistoricalCopy', new_historical_copy)
         # if node.historical is None:
         #     newHistorical = [node.config]
         #     print("New node historical ", newHistorical)
@@ -59,43 +56,48 @@ def getChilderNodes(node):
         #     newHistorical = node.historical.append(node.config)
         #     print("append node historical ", newHistorical)
 
-        childrenNodes.append(Node(config=config, historical=newHistoricalCopy))
-    return childrenNodes
-    
-def checkIfAnyNodeIsSolution(nodes):
+        children_nodes.append(Node(config=config, historical=new_historical_copy))
+    return children_nodes
+
+
+def check_any_node_is_solution(nodes):
     for node in nodes:
-        if isSameConfig(node.config, goalConfig):
+        if is_same_config(node.config, goal_config):
             return True, node
     return False, None
 
-def getChildrenConfigs(config=None):
+
+def get_children_configs(config=None):
     # Encontrar los vecinos del 0 (devolver [3,5.8])
-    possibleValuesToMove = getZeroNeighbors(config)
+    possible_values_to_move = get_zero_neighbors(config)
     
     children = []
 
     # Generar todas las posibles combinaciones
-    for valueToMove in possibleValuesToMove:
-        newConfig = switchValue(valueToMove, config)
-        children.append(newConfig)
+    for value_to_move in possible_values_to_move:
+        new_config = switch_value(value_to_move, config)
+        children.append(new_config)
     return children
 
-def getZeroNeighbors(config):
+
+def get_zero_neighbors(config):
     neighbors = find_neighbours(config)
     return next(item for item in neighbors if item["value"] == 0).get("neighbors")
 
-def switchValue(posValue, config):
-    print('intercambiando el valor', posValue)
-    zeroIndex = find_indexOfValue(0, config)
-    itemIndex = find_indexOfValue(posValue, config)
-    targetConfig = copy.deepcopy(config)
-    targetConfig[itemIndex[0]][itemIndex[1]] = 0
-    targetConfig[zeroIndex[0]][zeroIndex[1]] = posValue
-    return targetConfig
 
-def isSameConfig(someConfig, anotherConfig):
-    
-    return someConfig == anotherConfig
+def switch_value(pos_value, config):
+    print('intercambiando el valor', pos_value)
+    zero_index = find_index_value(0, config)
+    item_index = find_index_value(pos_value, config)
+    target_config = copy.deepcopy(config)
+    target_config[item_index[0]][item_index[1]] = 0
+    target_config[zero_index[0]][zero_index[1]] = pos_value
+    return target_config
+
+
+def is_same_config(some_config, another_config):
+    return some_config == another_config
+
 
 def find_neighbours(arr):
     neighbors = []
@@ -131,8 +133,10 @@ def find_neighbours(arr):
     return neighbors
 
 
-def find_indexOfValue(value, config):
+def find_index_value(value, config):
     index_array = [(index, row.index(value)) for index, row in enumerate(config) if value in row]
     return index_array[0]
 
-start()
+
+if __name__ == '__main__':
+    start()
---
+++ models/node.py
@@ -0,0 +1,4 @@
+class Node:
+    def __init__(self, config=None, historical=None):
+        self.config = config
+        self.historical = historical
